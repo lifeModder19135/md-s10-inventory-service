25ad65758ebd8de246c61284c3d49c0459eeb212
Nate Tolbert <ntolbertu85@gmail.com>
2021-04-27 03:43:00 -0400

    04272021 -- Project/Global -- INITIAL COMMIT - Project freshly extracted from Initializr;

---
+++ src/main/java/com/mdspicesmicro/kitcheninventoryservice/KitchenInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.kitcheninventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdspicesmicro/kitcheninventoryservice/KitchenInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.kitcheninventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
